b77c2594ce2ddda30ba6f8278d4703a7640268ac
yashvin1 <97937970+yashvin1@users.noreply.github.com>
2022-12-03 16:38:01 +0530

    adding ip

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,11 @@ from flask import Flask
 import socket
 app = Flask(__name__)
 hostname=socket.gethostname()
+IP=socket.gethostbyaddr()
 
 @app.route('/')
 def hello_name():
-	return f'Instance name :- {hostname}'
+	return f'Instance name :- {hostname}{IP}'
 
 if __name__ == '__main__':
-		app.run()
+      app.run()
